397d0005e95cba2135ebb09cfa9fd389bce621ec
Joesph Collins <collins.jw@btinternet.com>
2024-02-29 19:16:06 +0000

    auto alocate partner id to active id on first creation. Set to null if active partner profile is deleted

--- partner_profile/views.py
+++ partner_profile/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from main.permissions import IsPrimaryProfileOrReadOnly
 from rest_framework.permissions import IsAuthenticated
 from .models import PartnerProfile
+from user_profile.models import UserProfile
 from .serializers import PartnerProfileListSerializer, PartnerProfileDetailSerializer
 
 
@@ -17,8 +19,19 @@ class PartnerProfileList(generics.ListCreateAPIView):
         user = self.request.user
         return PartnerProfile.objects.filter(primary_profile=user).order_by('-created_at')
     
+    def perform_create(self, serializer):
+        # Save the PartnerProfile with the current user set as the primary_profile
+        serializer.save(primary_profile=self.request.user)
 
-class PartnerProfileDetail(generics.RetrieveUpdateAPIView):
+        # Check if this is the user's first partner profile
+        user_profile, created = UserProfile.objects.get_or_create(owner=self.request.user)
+        if user_profile.active_partner_profile_id is None:
+            # Set the active_partner_profile_id to the id of the newly created PartnerProfile
+            user_profile.active_partner_profile_id = serializer.instance.id
+            user_profile.save()
+    
+
+class PartnerProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     - Detail the specificly requested partner profile
     - Uses same Partner Profile serializer
@@ -31,3 +44,20 @@ class PartnerProfileDetail(generics.RetrieveUpdateAPIView):
     def get_queryset(self):
         user = self.request.user
         return PartnerProfile.objects.filter(primary_profile=user).order_by('-created_at')
+    
+    def destroy(self, request, *args, **kwargs):
+        partner_profile = self.get_object()
+        user_profile = get_object_or_404(UserProfile, owner=partner_profile.primary_profile)
+
+        # Check if the deleted PartnerProfile is the active one before performing the delete operation
+        is_active_profile = (user_profile.active_partner_profile_id == partner_profile.id)
+        
+        # Perform the deletion using the superclass's destroy method
+        response = super().destroy(request, *args, **kwargs)
+
+        # If the deleted PartnerProfile was the active one, update the UserProfile
+        if is_active_profile:
+            user_profile.active_partner_profile_id = None
+            user_profile.save()
+
+        return response
